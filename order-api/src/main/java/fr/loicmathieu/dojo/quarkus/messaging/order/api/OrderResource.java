edd4426d66f455990097e2e8bf4f72997465413a
Loïc Mathieu <loikeseke@gmail.com>
2020-11-18 13:54:59 +0100

    feat: improvements

--- order-api/src/main/java/fr/loicmathieu/dojo/quarkus/messaging/order/api/OrderResource.java
+++ order-api/src/main/java/fr/loicmathieu/dojo/quarkus/messaging/order/api/OrderResource.java
@@ -1,5 +1,8 @@
 package fr.loicmathieu.dojo.quarkus.messaging.order.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,8 +13,11 @@ import javax.ws.rs.core.MediaType;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class OrderResource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderResource.class);
+
     @POST
     public void create(Order order){
+        LOGGER.info("Creating order: {}", order);
         //TODO send the order to an 'orders' channel using an Emitter
     }
 }
